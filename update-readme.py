5b7d86e90b52c2e2180e9073353e55cd99e250e3
KrishnaRevanthKarra <krishnarevanth.ic@gmail.com>
2025-05-11 14:12:45 +0530

    Update update-readme.py

--- update-readme.py
+++ update-readme.py
@@ -26,7 +26,7 @@ response = requests.post(
 
 data = response.json()
 stats = {item["difficulty"]: item["count"] for item in data["data"]["matchedUser"]["submitStats"]["acSubmissionNum"]}
-langUsed = {item["languageName"] : item["problemsSolved"] for item in data["data"]["matchedUser"]["submitStats"]["languageProbelmCount"]}
+langUsed = {item["languageName"] : item["problemsSolved"] for item in data["data"]["matchedUser"]["submitStats"]["languageProblemCount"]}
 
 # Update README
 with open("README.md", "r") as file:
